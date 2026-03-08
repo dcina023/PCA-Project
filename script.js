e5aede7ed0a64a8ad70404af4b28e3224f3d7809
+ <+>
2026-03-07 20:08:32 -0500

    adding content to page

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const skills = ["HTML5", "CSS", "JavaScript"];
+function displaySkills() {
+    
+    const list = document.getElementById("skills-list");
+    skills.forEach(function(skill) {
+        const li = document.createElement("li");
+        li.textContent = skill;
+        list.appendChild(li);
+   });
+}
+
+displaySkills();
